838d0be96f3b15020663a50c5a7762b3aa2c48ab
leroygatlin <leroygatlin@gmail.com>
2023-11-06 17:27:55 -0800

    complete arrow functions

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+const square = parameter => parameter ** parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
